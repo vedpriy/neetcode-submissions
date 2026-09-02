effe3846f771c4ddba12b450ca3f52f3021a043a
vedpriy <p7180880@gmail.com>
2026-09-02 12:11:16 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+int f(int i,int amount,vector<int>&coins,vector<vector<int>>&dp){
+    if(i==0){
+        if(amount%coins[0]==0){
+            return 1;
+        }
+        return 0;;
+    }
+    if(dp[i][amount]!=-1){
+        return dp[i][amount];
+    }
+    int nottake=f(i-1,amount,coins,dp);
+    int take=0;
+    if(amount>=coins[i]){
+        take=f(i,amount-coins[i],coins,dp);
+    }
+    return dp[i][amount]=take+nottake;
+}
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+         return f(n-1,amount,coins,dp);
+
+    }
+};
